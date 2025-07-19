230900173aede8a07f204cab855aa91770ebc0f5
oskarweb <oskarweber123@gmail.com>
2025-07-19 15:39:18 +0200

    feature: cli initial implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "matrix.hpp"
 #include "utils.hpp"
 
+#include <boost/program_options.hpp>
 #include <CL/cl.h>
 
 #include <iostream>
@@ -11,8 +12,9 @@
 
 #define CHECK_ERROR(err, msg) if (err != CL_SUCCESS) { std::cerr << msg << ": " << err << std::endl; exit(1); }
 
+namespace boost_po = boost::program_options;
 
-int main() 
+int main(int argc, char *argv[]) 
 {
     std::cout << "Stack size: " << constants::getStackSize() << " bytes\n";
     const int N = 1024;
@@ -63,24 +65,55 @@ int main()
 
     // assert(allEqual(Res1, Res2, Res3, Res4, Res5));
 
-    Matrix<uint32_t, 32, 32> mat1{};
-    mat1.randomFill();
-    Matrix<uint32_t, 32, 32> mat2{};
-    mat2.randomFill();
-    auto res1 = mat1.mult<MultiplicationType::Naive>(mat2);
-    // std::cout << "Res1: " << '\n' << res1;
-    auto res2 = mat1.mult<MultiplicationType::Naive>(mat2);
-    // std::cout << "\nRes2: " << '\n' << res2;
-    auto res3 = mat1.mult<MultiplicationType::Simd>(mat2);
-    // std::cout << "\nRes3: " << '\n' << res3;
-    auto res4 = mat1.mult<MultiplicationType::MultithreadSimd>(mat2);
-    // std::cout << "\nRes4: " << '\n' << res4;
-    auto res5 = mat1.mult<MultiplicationType::MultithreadElement>(mat2);
-    // std::cout << "\nRes5: " << '\n' << res5;
-    auto res6 = mat1.mult<MultiplicationType::MultithreadRow>(mat2);
-    // std::cout << "\nRes6: " << '\n' << res6;
+    // Matrix<uint32_t, 32, 32> mat1{};
+    // mat1.randomFill();
+    // Matrix<uint32_t, 32, 32> mat2{};
+    // mat2.randomFill();
+    // auto res1 = mat1.mult<MultiplicationType::Naive>(mat2);
+    // // std::cout << "Res1: " << '\n' << res1;
+    // auto res2 = mat1.mult<MultiplicationType::Naive>(mat2);
+    // // std::cout << "\nRes2: " << '\n' << res2;
+    // auto res3 = mat1.mult<MultiplicationType::Simd>(mat2);
+    // // std::cout << "\nRes3: " << '\n' << res3;
+    // auto res4 = mat1.mult<MultiplicationType::MultithreadSimd>(mat2);
+    // // std::cout << "\nRes4: " << '\n' << res4;
+    // auto res5 = mat1.mult<MultiplicationType::MultithreadElement>(mat2);
+    // // std::cout << "\nRes5: " << '\n' << res5;
+    // auto res6 = mat1.mult<MultiplicationType::MultithreadRow>(mat2);
+    // // std::cout << "\nRes6: " << '\n' << res6;
+    try {
+        boost_po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("compression", boost_po::value<double>(), "set compression level")
+        ;
 
-    assert(util::allEqual(res1, res2, res3, res4, res5, res6));
+        boost_po::variables_map vm;        
+        boost_po::store(boost_po::parse_command_line(argc, argv, desc), vm);
+        boost_po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("compression")) {
+            std::cout << "Compression level was set to " 
+                 << vm["compression"].as<double>() << ".\n";
+        } else {
+            std::cout << "Compression level was not set.\n";
+        }
+    }
+    catch(std::exception& e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception of unknown type!\n";
+    }
+
+
+    // assert(util::allEqual(res1, res2, res3, res4, res5, res6));
 
     // return result;
     return 0;
